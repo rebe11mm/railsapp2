1e5e6717503f80eec5bc9afc3e464c6b56464c54
Rebecca McMurray <rebeccam.mcmurray@gmail.com>
2017-04-26 20:18:24 -0400

    5.1 Added dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   resources :products
-  get 'static_pages/about'
+  get 'static_pages/about', :as => 'about_page'
 
-  get 'static_pages/contact'
+  get 'static_pages/contact', :as => 'contact_page'
 
-  get 'static_pages/index'
+  get 'static_pages/index', :as => 'home_page'
 
-  root 'static_pages#landing_page'
+  get '/products', to: 'products#index' , :as => 'products_page'
+
+  root 'static_pages#index'
 
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
