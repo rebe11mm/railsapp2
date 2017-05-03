444f5ff96d84aab9132cd41f463dbc4914fff50f
Rebecca McMurray <rebeccam.mcmurray@gmail.com>
2017-05-03 14:40:18 -0400

    5.3 generating HTML emails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,13 +10,11 @@ class StaticPagesController < ApplicationController
   	@products = Product.limit(4)
 	end
 
-	def thank_you
-  	@name = params[:name]
-  	@email = params[:email]
-  	@message = params[:message]
-  	ActionMailer::Base.mail(:from => @email,
-      :to => 'rebeccam.mcmurray@gmail.com',
-      :subject => "A new contact form message from #{@name}",
-      :body => @message).deliver_now
-	end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
+  
 end
