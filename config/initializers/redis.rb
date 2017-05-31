38236eb9f76bc791b0e9e564f21a08f8e88133a6
Rebecca McMurray <rebeccam.mcmurray@gmail.com>
2017-05-31 08:00:53 -0400

    6.12 adding redis to heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,7 @@
 $redis = Redis.new(host: 'localhost', port: 6379)
+
+if Rails.env.production?
+	$redis = Redis.new(url: ENV["REDIS_URL"])
+else
+	$redis = Redis.new(:host => 'localhost', :port => 6379)
+end
