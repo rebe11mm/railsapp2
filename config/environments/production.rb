2e7627c3257583a17775059895c5d735ed784409
Rebecca McMurray <rebeccam.mcmurray@gmail.com>
2017-06-07 10:21:24 -0400

    6.13 adding Actioncable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://bike-berlin-2017.herokuapp.com/cable"
+config.action_cable.allowed_request_origins = ['https://bike-berlin-2017.herokuapp.com', 'http://bike-berlin-2017.herokuapp.com']
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,9 @@ Rails.application.routes.draw do
 
   root 'static_pages#landing_page'
 
-  resources :orders, only: [:index, :show, :create, :destroy]
+  mount ActionCable.server => '/cable'
 
+  resources :orders, only: [:index, :show, :create, :destroy]
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
