1b881b6d06b4e7cc917a0b64c4b2bf5652285939
Rebecca McMurray <rebeccam.mcmurray@gmail.com>
2017-06-20 09:08:15 -0400

    indentation 3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,34 +1,34 @@
 class Product < ApplicationRecord
-	has_many :orders
-	has_many :comments
-	validates :name, presence: true
+  has_many :orders
+  has_many :comments
+  validates :name, presence: true
 
-	def highest_rating_comment
-  	comments.rating_desc.first
-	end
+  def highest_rating_comment
+    comments.rating_desc.first
+  end
 
-	def lowest_rating_comment
-		comments.rating_asc.first
-	end
+  def lowest_rating_comment
+    comments.rating_asc.first
+  end
 
-	def self.search(search_term)
-  	if Rails.env.development?
-	  	Product.where("name LIKE ?", "%#{search_term}%")
-	 	else
-	  	Product.where("name ilike ?", "%#{search_term}%")
-	 	end
-	end
+  def self.search(search_term)
+    if Rails.env.development?
+      Product.where("name LIKE ?", "%#{search_term}%")
+    else
+      Product.where("name ilike ?", "%#{search_term}%")
+    end
+  end
 
-	def average_rating
-  	comments.average(:rating).to_f
-	end
+  def average_rating
+    comments.average(:rating).to_f
+  end
 
-	def views
-		$redis.get("product:#{id}")
-	end
+  def views
+    $redis.get("product:#{id}")
+  end
 
-	def viewed!
-		$redis.incr("product:#{id}")
-	end
+  def viewed!
+    $redis.incr("product:#{id}")
+  end
 end
 
