b65dccdbde4b9257fbc7c3b7b26eef3585cfe008
Rebecca McMurray <rebeccam.mcmurray@gmail.com>
2017-06-20 08:51:21 -0400

    indendtation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
-	def create
-		@product = Product.find(params[:product_id])
-		@comment = @product.comments.new(comment_params)
-		@comment.user = current_user
+  def create
+	  @product = Product.find(params[:product_id])
+	  @comment = @product.comments.new(comment_params)
+	  @comment.user = current_user
     @user = current_user
-		respond_to do |format|
+	  respond_to do |format|
       if @comment.save
         #ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
@@ -17,14 +17,14 @@ class CommentsController < ApplicationController
     end
   end
 
-	def destroy
-		@comment = Comment.find(params[:id])
-		product = @comment.product
-		@comment.destroy
-		redirect_to product
-	end
+  def destroy
+	  @comment = Comment.find(params[:id])
+	  product = @comment.product
+	  @comment.destroy
+	  redirect_to product
+  end
 
-	private
+  private
 
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
